1edc67368f890ec2f771c62c78a8004d1502e252
Hector <gt12930@GMAIL.com>
2024-10-10 21:20:55 -0500

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { message } from "antd";
 
-function App() {
+const RegistroAsistencia = () => {
+
+  const [dni, setDni] = useState("");
+  const [foto, setFoto] = useState(null);
+  const [mensaje, setMensaje] = useState("");
+
+  const handleDniChange = (e) => setDni(e.target.value);
+  const handleFotoChange = (e) => setFoto(e.target.files[0]);
+
+  const registrarAsistencia = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(async (position) => {
+        const latitud = position.coords.latitude;
+        const longitud = position.coords.longitude;
+
+        // Crear FormData para enviar datos al backend
+        const formData = new FormData();
+        formData.append("dni", dni);
+        formData.append("foto", foto); // La foto seleccionada por el usuario
+        formData.append("latitud", latitud);
+        formData.append("longitud", longitud);
+        
+        const response = await fetch(
+          "http://localhost:3001/api/v1/asistencia",
+          {
+            method: "POST",
+            body: formData,
+          }
+        );
+        const data = await response.json();
+        if (response.status === 200) {
+          message.success(data.mensaje);
+          setDni("");
+          setFoto(null);
+        } else {
+          message.error(data.mensaje);
+        }
+      });
+    } else {
+      console.log("Geolocalización no es soportada por este navegador.");
+    }
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h2 className="h2">Registro de Asistencia</h2>
+      <input
+        type="text"
+        placeholder="DNI"
+        value={dni}
+        onChange={handleDniChange}
+      />
+      <input type="file" onChange={handleFotoChange} accept="image/*" />
+      <button onClick={registrarAsistencia}>Registrar Asistencia</button>
     </div>
   );
-}
+};
 
-export default App;
+export default RegistroAsistencia;
