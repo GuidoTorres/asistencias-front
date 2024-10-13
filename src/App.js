ec0b7fcbf033a435749c35a2fc25d2aff4aceede
Hector <gt12930@GMAIL.com>
2024-10-13 08:25:04 -0500

    historial

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import { Anchor } from "antd";
 import MostrarAsistencias from "./components/MostrarAsistencias";
 import Asistencias from "./components/Asistencias";
-
 
 const App = () => {
   return (
@@ -11,22 +10,27 @@ const App = () => {
       <div style={{ padding: "20px" }}>
         {/* Ancla de navegación */}
         <Anchor
-        direction="horizontal"
-        items={[
-          {
-            key: 'part-1',
-            href:"/historial", title:"Historial de Asistencias"
-          },
-          {
-            key: 'part-2',
-            href:"/asistencias", title:"Registrar Asistencia"
-          },
-
-        ]}
-      />
+          direction="horizontal"
+          items={[
+            {
+              key: "part-1",
+              href: "/historial",
+              title: "Historial de Asistencias",
+            },
+            {
+              key: "part-2",
+              href: "/asistencias",
+              title: "Registrar Asistencia",
+            },
+          ]}
+        />
 
         {/* Contenido de las rutas */}
         <Routes>
+          {/* Redirigir desde la raíz ("/") hacia "/asistencias" */}
+          <Route path="/" element={<Navigate to="/asistencias" />} />
+
+          {/* Otras rutas */}
           <Route path="/historial" element={<MostrarAsistencias />} />
           <Route path="/asistencias" element={<Asistencias />} />
         </Routes>
